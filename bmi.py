a0eb731224175a121e8494db578716b3c19de50e
Wojtek Makos <wojtekmakos860@gmail.com>
2022-10-08 11:51:08 +0200

    initial commit

---
+++ bmi.py
@@ -0,0 +1,22 @@
+def calculate_bmi (weight, height):
+    bmi = weight/height ** 2
+    if bmi < 18.5:
+        return "underweight"
+    elif bmi > 20:
+        return "overweight"
+    return "normal"
+
+if __name__ == "__main__":
+    height = float(input("weight: "))
+    weight = float(input("height: "))
+
+    result = calculate_bmi(weight, height)
+    print("Your status: " + result)
+    print(f"Your status z fstringa: {result}")
+
+
+#else:
+#   if bmi > 20:
+#       result = "overweight"
+#  else:
+#     result = "normal"
