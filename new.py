dc9c56f2c3d2d6b9da9b1eed94083c263e018f57
Wojtek <wojtekmakos860@gmail.com>
2022-10-09 14:47:52 +0200

    first numbers

--- new.py
+++ new.py
@@ -1,12 +1,12 @@
 def is_prime(n):
-    i=3
-    if n == 1 or n == 2:
-        return True
-
-    for _ in range (n-1):
+    i=2
+    if n == 1:
+        return False
+    for _ in range (n-2):
         if n%i == 0:
             return False
+        i += 1
     return True
 
 if __name__ == "__main__":
-    print(is_prime(5))
+    print(is_prime(20))
