41e7732f8ca34dff5e776a34fb2fdc6db7d9ec6f
Wojtek <wojtekmakos860@gmail.com>
2022-10-09 13:23:19 +0200

    convert hex to dec and back

---
+++ hex.py
@@ -0,0 +1,54 @@
+def dec2hex(x):
+    if x == 0:
+        return "0"
+    result = ""
+
+    dictn = {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "A",
+        11: "B",
+        12: "C",
+        13: "D",
+        14: "E",
+        15: "F",
+    }
+    while x !=0:
+        x, reminder = divmod(x, 16)
+        result = str(dictn.get(reminder)) + result
+    return result
+
+def hex2dec(x):
+    dictn = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "A": 10,
+        "B": 11,
+        "C": 12,
+        "D": 13,
+        "E": 14,
+        "F": 15,
+    }
+    result = 0
+    for i, digit in enumerate(x):
+        result += dictn.get(digit) * 16 ** (len(x) - 1 - i)
+    return result
+
+
+if __name__ == "__main__":
+    print(hex2dec("ABC"))
+    print(dec2hex(2748))
