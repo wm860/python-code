16432b6ad30f3bbc2ba558b8b15634d812e10f14
Wojtek <wojtekmakos860@gmail.com>
2022-10-09 11:29:24 +0200

    dictionary,more features

--- slownik.py
+++ slownik.py
@@ -7,3 +7,30 @@ if __name__ == "__main__":
     print(phones)
     phones["Justin"] = "555555"
     print(phones)
+
+    print(phones.keys())
+    print(phones.values())
+
+    for key in phones:
+        print(key)
+    for value in phones.values():
+        print(value)
+
+    for key in phones:
+        print(key, phones[key])
+
+    for key, value in phones.items():
+        print(key, value)
+
+
+    prev_phones = phones.copy()
+    print(phones.get("900"))
+    print(phones.get("50", "No value"))
+
+    phones.update({"will": "numer"})
+
+    phones.pop("will")
+
+    phones.pop("will")
+
+
