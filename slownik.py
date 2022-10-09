82a1679ed7dfd91ae7c0a7d1bc7382d5e5957c11
Wojtek <wojtekmakos860@gmail.com>
2022-10-09 10:21:20 +0200

    dictionary type

---
+++ slownik.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    phones = {
+        "Bob Marley" : "11-22-33",
+        "Another Guy" : "44 - 55 - 11",
+        "Justin" : None
+    }
+    print(phones)
+    phones["Justin"] = "555555"
+    print(phones)
