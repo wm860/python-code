eba2e08be42f349cd53e49a7012b7d4ff0979e67
Wojtek <wojtekmakos860@gmail.com>
2022-10-15 14:33:28 +0200

    2. freq counting frequency words - the easiest one

--- freq.py
+++ freq.py
@@ -1,9 +1,6 @@
 from collections import defaultdict
 
 def calc_freq(sentence):
-    freq = defaultdict(int)
-    for word in sentence.lower().split():
-        freq[word] += 1
-    return freq
+    return Counter(sentence.lower().split())
 if __name__ == "__main__":
     print(calc_freq("This is a simple text this"))
